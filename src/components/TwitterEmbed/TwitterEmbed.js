72d33e00a056fb80dd5cb346d50d87b52f4e4d8c
Jakub Dreczkowski <kubadreczz@gmail.com>
2020-03-31 23:26:20 +0200

    Remove TwEmbed appear animation

--- src/components/TwitterEmbed/TwitterEmbed.js
+++ src/components/TwitterEmbed/TwitterEmbed.js
@@ -1,40 +1,12 @@
 import React from "react"
 import { TwitterTimelineEmbed } from 'react-twitter-embed'
 import './TwitterEmbed.scss'
-import { useIntersection } from 'react-use'
-import { motion } from 'framer-motion'
-
-
-const variants = {
-  hidden: {
-    opacity: 0
-  },
-  visible: {
-    opacity: 1,
-    transition: {
-      duration: 0.4,
-      ease: 'easeInOut'
-    }
-  }
-}
 
 const TwitterEmbed = () => {
 
-  const sectionRef = React.useRef(null)
-  const intersection = useIntersection(sectionRef, {
-     root: null,
-     rootMargin: "0px",
-     threshold: 0.1
-  })
-
   return (
     <div className="twitter-embed-container">
-      <motion.div
-        ref={sectionRef}
-        variants={variants}
-        initial="hidden"
-        animate={(intersection && intersection.isIntersecting) ? "visible" : "hidden"}
-        >
+      <div>
         <div>
           <div className="mz-title">Aktualności Ministerstwa Zdrowia</div>
           <div className="twitter-mz-embed">
@@ -48,7 +20,7 @@ const TwitterEmbed = () => {
               />
           </div>
         </div>
-      </motion.div>
+      </div>
     </div>
   )
 }
